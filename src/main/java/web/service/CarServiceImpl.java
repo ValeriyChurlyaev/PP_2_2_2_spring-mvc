df8410332ee52252208b82f46bf80f652b6bf7a2
ValeriyChurlyaev <chyrlyaevva@gmail.com>
2022-10-03 12:15:58 +0300

    (fix 1)

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,25 +1,24 @@
 package web.service;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import web.dao.CarDAO;
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CarServiceImpl implements CarService {
 
-    private final CarDAO carDAO;
-
-    public CarServiceImpl(CarDAO carDAO) {
-        this.carDAO = carDAO;
-    }
-
 
     @Override
     public List<Car> carList(int num) {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car(137, "White", "Tesla"));
+        cars.add(new Car(164, "Red", "Mustang"));
+        cars.add(new Car(337, "Black", "Porsche"));
+        cars.add(new Car(820, "Nightblue", "Cadillac"));
+        cars.add(new Car(954, "Green", "Fiat"));
 
-        return carDAO.CarList().stream().limit(num).toList();
+        return cars.stream().limit(num).toList();
     }
 }
